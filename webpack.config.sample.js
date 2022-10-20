8239e80a62ab6f5251300fc1277c5bdc4db19f80
Josh Reep <joshreep@gmail.com>
2022-10-20 15:24:05 -0400

    stuff for page build

--- webpack.config.sample.js
+++ webpack.config.sample.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   target: 'web',
   entry: './src/App.tsx',
   module: {
