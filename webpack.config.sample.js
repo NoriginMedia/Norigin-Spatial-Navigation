57db7478effd41557254bfdea93246c502ec8136
Josh Reep <joshreep@gmail.com>
2022-10-20 15:27:36 -0400

    sample artifacts

--- webpack.config.sample.js
+++ webpack.config.sample.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'sample'),
     globalObject: 'this'
   },
   plugins: [
