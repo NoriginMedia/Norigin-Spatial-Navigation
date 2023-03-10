9d08a168be58da99f27e10e1bf67925079583676
Hans <hans00@users.noreply.github.com>
2023-03-10 18:10:39 +0800

    Use `getBoundingClientRect` for measure layout (#61)
    * Use `getBoundingClientRect` instead loop
    
    * Revert "Use `getBoundingClientRect` instead loop"
    
    This reverts commit a28193b78fe734f214f3cffc03db3391efffe97f.
    
    * Make `useGetBoundingClientRect` as init option
    
    * Fix lint error

--- src/SpatialNavigation.ts
+++ src/SpatialNavigation.ts
@@ -7,7 +7,7 @@ import forEach from 'lodash/forEach';
 import forOwn from 'lodash/forOwn';
 import throttle from 'lodash/throttle';
 import difference from 'lodash/difference';
-import measureLayout from './measureLayout';
+import measureLayout, { getBoundingClientRect } from './measureLayout';
 import VisualDebugger from './VisualDebugger';
 
 const DIRECTION_LEFT = 'left';
@@ -197,6 +197,11 @@ class SpatialNavigationService {
    * Flag used to block key events from this service
    */
   private paused: boolean;
+
+  /**
+   * Enables/disables getBoundingClientRect
+   */
+  private useGetBoundingClientRect: boolean;
 
   private keyDownEventListener: (event: KeyboardEvent) => void;
 
@@ -520,6 +525,7 @@ class SpatialNavigationService {
     this.nativeMode = false;
     this.throttle = 0;
     this.throttleKeypresses = false;
+    this.useGetBoundingClientRect = false;
 
     this.pressedKeys = {};
 
@@ -556,12 +562,14 @@ class SpatialNavigationService {
     visualDebug = false,
     nativeMode = false,
     throttle: throttleParam = 0,
-    throttleKeypresses = false
+    throttleKeypresses = false,
+    useGetBoundingClientRect = false
   } = {}) {
     if (!this.enabled) {
       this.enabled = true;
       this.nativeMode = nativeMode;
       this.throttleKeypresses = throttleKeypresses;
+      this.useGetBoundingClientRect = useGetBoundingClientRect;
 
       this.debug = debug;
 
@@ -1403,8 +1411,12 @@ class SpatialNavigationService {
 
     const { node } = component;
 
+    const layout = this.useGetBoundingClientRect
+      ? getBoundingClientRect(node)
+      : measureLayout(node);
+
     component.layout = {
-      ...measureLayout(node),
+      ...layout,
       node
     };
   }
--- src/measureLayout.ts
+++ src/measureLayout.ts
@@ -44,3 +44,20 @@ const measureLayout = (node: HTMLElement) => {
 };
 
 export default measureLayout;
+
+export const getBoundingClientRect = (node: HTMLElement) => {
+  if (node && node.getBoundingClientRect) {
+    const rect = node.getBoundingClientRect();
+
+    return {
+      x: rect.x,
+      y: rect.y,
+      width: rect.width,
+      height: rect.height,
+      left: rect.left,
+      top: rect.top
+    };
+  }
+
+  return { x: 0, y: 0, width: 0, height: 0, left: 0, top: 0 };
+};
