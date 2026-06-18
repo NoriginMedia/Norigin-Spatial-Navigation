1c5119c2539da0ac71cc49badc1fb2350aef3ecc
Javier López <xavi160@users.noreply.github.com>
2026-06-18 15:34:53 +0200

    Add extra compatibility for web bundlers like Vite or Webpack when using with React Native Web (#230)
    * feat: enhance rollup configuration and add web-specific entry point
    
    - Updated rollup configuration to support separate builds for web and common JS formats.
    - Added new entry point for web-specific functionality in src/index.web.ts.
    - Updated package.json exports to include web module paths.

--- packages/react-native-tvos/rollup.config.mjs
+++ packages/react-native-tvos/rollup.config.mjs
@@ -4,12 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 
-export default defineConfig({
-  input: 'src/index.ts',
-  output: [
-    { file: 'dist/index.mjs', format: 'es' },
-    { file: 'dist/index.cjs', format: 'cjs' }
-  ],
+const baseConfig = {
   external: [
     '@noriginmedia/norigin-spatial-navigation-core',
     '@noriginmedia/norigin-spatial-navigation-react',
@@ -18,4 +13,23 @@ export default defineConfig({
     'react-native'
   ],
   plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
-});
+};
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: 'dist/index.mjs', format: 'es' },
+      { file: 'dist/index.cjs', format: 'cjs' }
+    ],
+    ...baseConfig
+  },
+  {
+    input: 'src/index.web.ts',
+    output: [
+      { file: 'dist/index.web.mjs', format: 'es' },
+      { file: 'dist/index.web.cjs', format: 'cjs' }
+    ],
+    ...baseConfig
+  }
+]);
